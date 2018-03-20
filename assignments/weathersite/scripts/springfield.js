677170122695f5254df537456d81c59bb08d8acf
Amy Zuniga <35243042+amzuni4@users.noreply.github.com>
2018-03-20 12:26:38 -0700

    d

--- assignments/weathersite/scripts/springfield.js
+++ assignments/weathersite/scripts/springfield.js
@@ -12,7 +12,7 @@ SweatherObject.onload = function () {
     document.getElementById('ScurrentTemp').innerHTML = weatherInfo.current_observation.temp_f;
     document.getElementById('SweathObs').innerHTML = weatherInfo.current_observation.weather;
     document.getElementById('Sweather_icon').src = weatherInfo.current_observation.icon_url;
- document.getElementById('SwindChill').innerHTML = weatherInfo.current_observation.windchill_f;
+    document.getElementById('SwindChill').innerHTML = weatherInfo.current_observation.windchill_f;
 
     document.getElementById('SwindMPH').innerHTML = weatherInfo.current_observation.wind_mph;
     document.getElementById('Sforecast').innerHTML = weatherInfo.current_observation.feelslike_string;
@@ -22,17 +22,17 @@ SweatherObject.onload = function () {
 }
 
 var StxtForecast = new XMLHttpRequest();
-StxtForecast.open('GET','https://api.wunderground.com/api/9d9d50d9dae82d0f/forecast10day/q/OR/Springfield.json', true);
+StxtForecast.open('GET', 'https://api.wunderground.com/api/9d9d50d9dae82d0f/forecast10day/q/OR/Springfield.json', true);
 
 StxtForecast.send();
 
-StxtForecast.onload = function() {
+StxtForecast.onload = function () {
     var weatherText = JSON.parse(StxtForecast.responseText);
     console.log(weatherText);
 
     document.getElementById('Shi').innerHTML = weatherText.forecast.txt_forecast.forecastday["0"].fcttext;
-    
-    var weatherTen = JSON.parse(StxtForecast.responseText).forecast.simpleforecast.forecastday;    
+
+    var weatherTen = JSON.parse(StxtForecast.responseText).forecast.simpleforecast.forecastday;
     var tbody = document.querySelector('tbody');
     var tr = document.createElement('tr');
 
@@ -44,6 +44,6 @@ StxtForecast.onload = function() {
 
     }
     console.log(weatherTen);
-    
+
 
 }
