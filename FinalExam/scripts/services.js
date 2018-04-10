bc9209f8be4bca0324e8e06028a647ab3fe65251
Amy Zuniga <35243042+amzuni4@users.noreply.github.com>
2018-04-10 16:39:28 -0700

    f

--- FinalExam/scripts/services.js
+++ FinalExam/scripts/services.js
@@ -1,60 +1,73 @@
- $(document).ready(function () {
-        $.getJSON("data.json", function (data) {
+   function CreateTableFromJSON() {
+        var myTable = [
+            {
+                "Repairs": "Tire Replacement",
+                "Category": "Maintainence",
+                "Price": "$125.60"
+            },
+             {
+                "Repairs": "Chain Cleaning",
+                "Category": "Maintainence",
+                "Price": "$25.60"
+            },
+             {
+                "Repairs": "Wheel Alignment",
+                "Category": "Maintainence",
+                "Price": "$95.60"
+            },
+            
+             {
+                "Repairs": "Shifter Adjustment",
+                "Category": "Maintainence",
+                "Price": "$125.60"
+            },
+             {
+                "Repairs": "Complete Bicycle Overhaul",
+                "Category": "Maintainence",
+                "Price": "$225.60"
+            }
+        ]
 
-            var arrItems = [];      // THE ARRAY TO STORE JSON ITEMS.
-            $.each(data, function (index, value) {
-                arrItems.push(value);       // PUSH THE VALUES INSIDE THE ARRAY.
-            });
-
-            // EXTRACT VALUE FOR TABLE HEADER.
-            var col = [];
-            for (var i = 0; i < arrItems.length; i++) {
-                for (var key in arrItems[i]) {
-                    if (col.indexOf(key) === -1) {
-                        col.push(key);
-                    }
+        // EXTRACT VALUE FOR HTML HEADER. 
+        // ('Book ID', 'Book Name', 'Category' and 'Price')
+        var col = [];
+        for (var i = 0; i < myTable.length; i++) {
+            for (var key in myTable[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
                 }
             }
+        }
 
-            // CREATE DYNAMIC TABLE.
-            var table = document.createElement("table");
+        // CREATE DYNAMIC TABLE.
+        var table = document.createElement("table");
 
-            // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
 
-            var tr = table.insertRow(-1);                   // TABLE ROW.
+        var tr = table.insertRow(-1);                   // TABLE ROW.
 
-            for (var i = 0; i < col.length; i++) {
-                var th = document.createElement("th");      // TABLE HEADER.
-                th.innerHTML = col[i];
-                tr.appendChild(th);
+        for (var i = 0; i < col.length; i++) {
+            var th = document.createElement("th");      // TABLE HEADER.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+        }
+
+        // ADD JSON DATA TO THE TABLE AS ROWS.
+        for (var i = 0; i < myTable.length; i++) {
+
+            tr = table.insertRow(-1);
+
+            for (var j = 0; j < col.length; j++) {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = myTable[i][col[j]];
             }
+        }
 
-            // ADD JSON DATA TO THE TABLE AS ROWS.
-            for (var i = 0; i < arrItems.length; i++) {
-
-                tr = table.insertRow(-1);
-
-                for (var j = 0; j < col.length; j++) {
-                    var tabCell = tr.insertCell(-1);
-                    tabCell.innerHTML = arrItems[i][col[j]];
-                }
-            }
-
-            // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-            var divContainer = document.getElementById("showData");
-            divContainer.innerHTML = "";
-            divContainer.appendChild(table);
-        });
-    });
-
-
-
-
-
-
-
-
-
+        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+        var divContainer = document.getElementById("showData");
+        divContainer.innerHTML = "";
+        divContainer.appendChild(table);
+    }
 
 
 
